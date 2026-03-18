307b2b4db0030d0b96cb34338f10a55867b6c803
PC <tobias.irigoyen@gmail.com>
2026-03-18 19:39:06 -0300

    (setup): upload new build

--- dist/fluxia-ai/server/angular-app-manifest.mjs
+++ dist/fluxia-ai/server/angular-app-manifest.mjs
@@ -17,9 +17,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 20258, hash: '43651bdd16fab222141d5b6c2d562ff2d4326f7af9f729d7fb0927314e02c50d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 13664, hash: 'e394fbc2be512f5767a6d17195c8feed7efb6058c65a8769bed0473c81da56a0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 132068, hash: 'd5a3996c5b7dad27cf8f9c1029b330f995297162a12108acd354d45c2789c8b6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 20223, hash: 'ef46c3498656c476cb0c4c086f1768f868f623770a5edb019c27d5ae11e4d515', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 13629, hash: '76b7609c05833986d9ab59cd6ed549ff0763cad380b343298d62ebb579eb74c0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 132033, hash: 'f019bb436ddaab728ce775f9d71326d5c1ed358f060cfec8b2be892614444927', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-GV65MC6R.css': {size: 28549, hash: 'bzpKmc3WGcs', text: () => import('./assets-chunks/styles-GV65MC6R_css.mjs').then(m => m.default)}
   },
 };
